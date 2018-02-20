10e1936af34fbf90be8b4ed73d79f1e941498173
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-02-20 17:17:39 -0300

    Administrador cadastra cozinhas (em andamento)

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -2,4 +2,21 @@ class CuisinesController < ApplicationController
   def show
     @cuisine = Cuisine.find(params[:id])
   end
+
+  def new
+    @cuisine = Cuisine.new
+  end
+
+  def create
+    name = params[:cuisine][:name]
+
+    cuisine = Cuisine.new(name: name)
+
+    if cuisine.save
+      @last_cuisine = Cuisine.last.id
+      redirect_to cuisine_url(@last_cuisine)
+    else
+      redirect_to new_cuisine_url
+    end
+  end
 end
