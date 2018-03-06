7fe59a7bc73a781c4db29f341bbc8a362dae6a57
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-03-06 18:25:37 -0300

    Visitante busca receitas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
-  get '/search', to: 'home#search', as: 'search'
+  get 'recipes/search', to: 'recipes#search', as: 'search'
   resources :recipes, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :cuisines, only: [:show, :new, :create, :edit, :update]
   resources :recipe_types, only: [:show, :new, :create]
