7ad9716eea693e6ac722a71885b46350aeec54c6
Glauco Elias Margarido <glauco.margarido@gmail.com>
2018-03-08 18:48:05 -0300

    [WIP] Usuário se autentica no site

--- spec/features/user_can_authenticate_spec.rb
+++ spec/features/user_can_authenticate_spec.rb
@@ -2,16 +2,33 @@ require 'rails_helper'
 
 feature 'User can authenticate' do
   scenario 'successfully' do
-    User.create(email: 'felix@gmail.com', password: '12345678')
+    user = User.create(email: 'felix@gmail.com', password: '12345678')
 
     visit root_path
     click_on 'Entrar'
-    fill_in 'Email', with: 'felix@gmail.com'
-    fill_in 'Senha', with: '12345678'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
     click_on 'Logar'
 
     expect(page).to have_css('nav', text: "Bem-vindo felix@gmail.com")
     expect(page).not_to have_link('Entrar')
     expect(page).to have_link('Sair')
+  end
+  scenario 'and logout' do
+    user = User.create(email: 'felix@gmail.com', password: '12345678')
+
+    visit root_path
+    click_on 'Entrar'
+    fill_in 'Email', with: user.email
+    fill_in 'Senha', with: user.password
+    click_on 'Logar'
+
+    expect(page).to have_css('nav', text: "Bem-vindo felix@gmail.com")
+    expect(page).to have_link('Entrar')
+    expect(page).not_to have_link('Sair')
+  end
+
+  scenario 'and view the recipe owner' do
+
   end
 end
